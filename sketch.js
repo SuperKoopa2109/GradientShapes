3b4c68a089c3bd45257ad08347f5c31370bfc64d
SuperKoopa2109 <niklaskaspareit@gmail.com>
2022-06-04 22:40:37 +0200

    added commenting and removed unnecessary code

--- sketch.js
+++ sketch.js
@@ -12,12 +12,13 @@ var gridResolutionMin = 3;
 var gridResolutionMax = 15;
 var gridResolutionStep = 1;
 
+var gradientTriangleRes = 255;
+
 var ballSpeed = 0;
 var ballRadius = 5;
 
 function setup() {
   createCanvas(600, 600);
-	//colorMode(HSB);
 	frameRate(60);
   for(let i=0;i<3; i++){
     balls.push(new Ball(round(random(0,width)), round(random(0,height))));
@@ -31,6 +32,8 @@ function setup() {
 	gui.addGlobals("ballSpeed");
 	sliderRange(5,70,1);
 	gui.addGlobals("ballRadius");
+	sliderRange(10, 1000, 5);
+	gui.addGlobals("gradientTriangleRes");
 	ellipseMode(CENTER);
 }
 
@@ -44,43 +47,22 @@ function draw() {
 	
   for(let ball of balls){
 		ball.update();
-    //ball.display();
 	drawGradientRect();
-	//ball.displayLines();
   }
-	//drawGradientBackground();
 	
-}
-
-function radialGradient(sX, sY, sR, eX, eY, colorS, colorE){
-	let gradient = drawingContext.createRadialGradient(
-	sX, sY, sR, eX, eY
-	);
 }
 
 function drawGradientRect(){  
-	c1 = color(255, 0, 0);
-	c2 = color(0, 255, 0);
-	//setGradient(c1, c2);
-	//drawingContext.createRadialGradient();
-	//setGradient(50, 90, 540, 80, c1, c2, Y_AXIS);
-	//fill(255, 204, 0);
-	describe('yellow rect with black outline in center of canvas');
-	//rect(20, 20, 60, 60);
-	//ellipse(200, 200, 50);
 	
+	// If n == 3 a gradient triangle will be created between points
 	if (balls.length == 3) {
-
-		//for ( let i = 0; i < 255; i++ ){
-				
-		//}
 		
-		//stroke(255,0,0);
-		//line(10,10,100,100);
+		// init points
 		var A = createVector(balls[0].pos.x, balls[0].pos.y );
 		var B = createVector(balls[1].pos.x, balls[1].pos.y );
 		var C = createVector(balls[2].pos.x, balls[2].pos.y );
-		
+
+		// compute vectors between points
 		var AB = createVector(B.x - A.x, B.y - A.y);
 		var AC = createVector(C.x - A.x, C.y - A.y);
 		var BC = createVector(C.x - B.x, C.y - B.y);
@@ -91,18 +73,15 @@ function drawGradientRect(){
 		
 
 		
-		for ( let i = 0; i < 255; i++ ) {
+		for ( let i = 0; i < gradientTriangleRes; i++ ) {
 			
-			A = createVector(balls[0].pos.x + AC.x * ( i/255 ), balls[0].pos.y + AC.y * ( i/255 ) ); //createVector(0,0);
-			B = createVector(balls[1].pos.x + BC.x * ( i/255 ), balls[1].pos.y + BC.y * ( i/255 ) );//createVector(0,-5);
-			C = createVector(balls[2].pos.x, balls[2].pos.y );//createVector(8,0);
-			
-			// calc vectors
-			//AB = createVector(balls[0].pos.x - balls[1].pos.x, balls[0].pos.y - balls[1].pos.y);
-			//AC = createVector(balls[0].pos.x - balls[2].pos.x, balls[0].pos.y - balls[2].pos.y);
-			//BC = createVector(balls[1].pos.x - balls[2].pos.x, balls[1].pos.y - balls[2].pos.y);
+			// compute new points on line between points
+			A = createVector(balls[0].pos.x + AC.x * ( i/gradientTriangleRes ), balls[0].pos.y + AC.y * ( i/gradientTriangleRes ) );
+			B = createVector(balls[1].pos.x + BC.x * ( i/gradientTriangleRes ), balls[1].pos.y + BC.y * ( i/gradientTriangleRes ) );
+			// Point C remains fixed
+			C = createVector(balls[2].pos.x, balls[2].pos.y );
 
-			
+/*			// Currently no calculation of height necessary
 			// calc determinant
 			det = A.x * ( ( B.y * 1 ) - ( C.y * 1 ) ) - A.y * ( ( B.x * 1 ) - ( C.x * 1 ) ) + 1 * ( ( B.x * C.y ) - ( C.x * B.y ) );
 			
@@ -110,100 +89,26 @@ function drawGradientRect(){
 			Area = 1/2 * det;
 			
 			// calc height
-			h = ( 2 * Area ) / L2_Norm(AC);
-			
-			//strokeWeight(20);
-			//stroke(255,0,0);
-			//console.log([A.x, A.y, B.x, B.y]);
+			h = ( 2 * Area ) / L2_Norm(AC); */
 			
 			let gradient = drawingContext.createLinearGradient(
 				A.x, A.y, B.x, B.y
 			);
 			
-			gradient.addColorStop(0, color(255 * ( 1 - ( i / 255 ) ), 0, i));
-			gradient.addColorStop(1, color(0, 255 * ( 1 - ( i / 255 ) ), i));
+			gradient.addColorStop(0, color(255 * ( 1 - ( i / gradientTriangleRes ) ), 0, 255 * ( i / gradientTriangleRes )));
+			gradient.addColorStop(1, color(0, 255 * ( 1 - ( i / gradientTriangleRes ) ), 255 * ( i / gradientTriangleRes )));
 			
 			drawingContext.strokeStyle = gradient;
 			
 			line(A.x, A.y, B.x, B.y);
-			fill(255, 0, 0);
 		}
 	}
 	stroke(0,0,0);
 	
-	
-	/* d_x = round(balls[0].pos.x - balls[1].pos.x);
-	strokeWeight(5);
-	//line(30, 20, 85, 20);
-	//stroke(0,255,0);
-	//line(50,50, 1000, 1000);
-	//line(round(balls[0].pos.x), round(balls[0].pos.y), round(balls[1].pos.x), round(balls[1].pos.y));
-	for ( i = balls[0].pos.x; i < balls[1].pos.x; i++ ){
-		stroke(255,0,0);
-		//line(
-	}
-	//console.log(balls.length);
-	//console.log(round(balls[0].pos.x - balls[1].pos.x));
-	for (let ball of balls) {
-		//ball.pos.x 
-		for( let x = 0; x<100; x+=1){
-			fill(x,22,22);
-		}
-	} */
 }
 
 function L2_Norm(vec){
 	return Math.sqrt(Math.pow(vec.x, 2) + Math.pow(vec.y, 2) );
-}
-
-/* function setGradient(x, y, w, h, c1, c2, axis) {
-  noFill();
-
-  if (axis === Y_AXIS) {
-    // Top to bottom gradient
-    for (let i = y; i <= y + h; i++) {
-      let inter = map(i, y, y + h, 0, 1);
-      let c = lerpColor(c1, c2, inter);
-      stroke(c);
-      line(x, i, x + w, i);
-    }
-  } else if (axis === X_AXIS) {
-    // Left to right gradient
-    for (let i = x; i <= x + w; i++) {
-      let inter = map(i, x, x + w, 0, 1);
-      let c = lerpColor(c1, c2, inter);
-      stroke(c);
-      line(i, y, i, y + h);
-    }
-  }
-} */
-
-function setGradient(c1, c2) {
-  // noprotect
-  noFill();
-  for (var y = 0; y < height; y++) {
-    var inter = map(y, 0, height, 0, 1);
-    var c = lerpColor(c1, c2, inter);
-    stroke(c);
-    line(0, y, width, y);
-  }
-}
-
-function drawGradientBackground(){
-	let index = 0;
-	let sum ;
-	for(let y=0;y<height;y+=gridResolution){
-		for(let x=0;x<width;x+=gridResolution){
-			sum = 0;
-			for(let ball of balls){
-				let d = dist(x,y,ball.pos.x, ball.pos.y);
-				let col = 400*ballRadius/d;
-				sum+=col;
-			}
-			fill(sum,255,255);
-			rect(x,y,gridResolution, gridResolution);
-		}
-	}
 }
 
 
